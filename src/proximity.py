9fb3c36f0db8bc134a4ec1eebb05b3f64bf54203
Feodor <zimfv@yandex.ru>
2025-01-02 19:17:28 +0100

    Correct Proximity.__add__; Wrote and test proximity_distance in notebook

--- src/proximity.py
+++ src/proximity.py
@@ -3,7 +3,6 @@ import numpy as np
 from src.depth import DepthPoset
 
 from matplotlib import pyplot as plt
-from matplotlib import cm
 
 class Proximity:
 	def __init__(self, x, y, values):
@@ -59,18 +58,43 @@ class Proximity:
 	def from_depth_poset(self, dp: DepthPoset):
 		"""
 		"""
-		xmin, xmax, ymin, ymax = np.array([(e0.birth, e1.birth, e0.death, e1.death) for e0, e1 in dp.edges]).transpose()
+		xmin, xmax, ymin, ymax = np.array([(e1.birth_value, e0.birth_value, e0.death_value, e1.death_value) for e0, e1 in dp.edges]).transpose()
 		return Proximity.from_rectangles(xmin, xmax, ymin, ymax, values=1)
+
+	def __call__(self, x, y):
+		"""
+		"""
+		x = np.asarray(x)
+		y = np.asarray(y)
+
+		if x.shape != y.shape:
+			raise ValueError(f'Arrays should have the same shape, but: x.shape={x.shape} and y.shape={y.shape}')
+		if len(x.shape) != 1:
+			return self(x.ravel(), y.ravel()).reshape(x.shape)
+
+		# Создаем булевы массивы, определяющие, в какие интервалы попадают x и y
+		x_mask = (self.x[:-1, None] <= x) & (self.x[1:, None] > x)
+		y_mask = (self.y[:-1, None] <= y) & (self.y[1:, None] > y)
+
+		# Находим индексы интервалов
+		x_indices = np.argmax(x_mask, axis=0)
+		y_indices = np.argmax(y_mask, axis=0)
+
+		# Возвращаем соответствующие значения
+		return self.values[x_indices, y_indices]
 
 	def __add__(self, other):
 		"""
 		"""
-		xmin = np.concatenate([np.tile(self.x[:-1], len(self.y) - 1), np.tile(other.x[:-1], len(other.y) - 1)])
-		xmax = np.concatenate([np.tile(self.x[1: ], len(self.y) - 1), np.tile(other.x[1: ], len(other.y) - 1)])
-		ymin = np.concatenate([np.tile(self.y[:-1], len(self.x) - 1), np.tile(other.y[:-1], len(other.x) - 1)])
-		ymax = np.concatenate([np.tile(self.y[1: ], len(self.x) - 1), np.tile(other.y[1: ], len(other.x) - 1)])
-		values = np.concatenate([np.concatenate(self.values), np.concatenate(other.values)])
-		return Proximity.from_rectangles(xmin, xmax, ymin, ymax, values)
+		x = np.unique(np.concatenate([self.x, other.x]))[1:-1]
+		y = np.unique(np.concatenate([self.y, other.y]))[1:-1]
+
+		x_grid = np.append(x - 0.5*(x[1:] - x[:-1]).min(), x[-1] + 1)
+		y_grid = np.append(y - 0.5*(y[1:] - y[:-1]).min(), y[-1] + 1)
+		y_grid, x_grid = np.meshgrid(y_grid, x_grid)
+		values = self(x_grid, y_grid) + other(x_grid, y_grid)
+
+		return Proximity(x, y, values)
 
 	def __mul__(self, num):
 		"""
@@ -92,29 +116,6 @@ class Proximity:
 		"""
 		return Proximity(self.x, self.y, self.values**num)
 
-	def __call__(self, x, y):
-		"""
-		"""
-		#return self.values[(self.x[:-1] <= x)&(self.x[1:] > x), (self.y[:-1] <= y)&(self.y[1:] > y)][0]
-		x = np.asarray(x)
-		y = np.asarray(y)
-
-		if x.shape != y.shape:
-			raise ValueError(f'Arrays should have the same length, but: x.shape={x.shape} and y.shape={y.shape}')
-		if len(x.shape) > 1:
-			return self(x.ravel(), y.ravel()).reshape(x.shape)
-
-		# Создаем булевы массивы, определяющие, в какие интервалы попадают x и y
-		x_mask = (self.x[:-1, None] <= x) & (self.x[1:, None] > x)
-		y_mask = (self.y[:-1, None] <= y) & (self.y[1:, None] > y)
-
-		# Находим индексы интервалов
-		x_indices = np.argmax(x_mask, axis=0)
-		y_indices = np.argmax(y_mask, axis=0)
-
-		# Возвращаем соответствующие значения
-		return self.values[x_indices, y_indices]
-
 	def min(self):
 		"""
 		"""
@@ -125,10 +126,10 @@ class Proximity:
 		"""
 		return self.values.max()
 
-	def show(self, xmin=0, xmax=1, ymin=0, ymax=1, cmap='winter', alpha=None, vmin=None, vmax=None, ax=plt, **kwargs):
+	def show(self, xmin=-np.inf, xmax=np.inf, ymin=-np.inf, ymax=np.inf, cmap='winter', alpha=None, vmin=None, vmax=None, ax=plt, ignore=0, **kwargs):
 		"""
 		"""
-		cmap = cm.get_cmap(cmap)
+		cmap = plt.get_cmap(cmap)
 
 		if vmin is None:
 			vmin = self.min()
@@ -143,11 +144,18 @@ class Proximity:
 				x0, x1, y0, y1 = x[ix], x[ix + 1], y[iy], y[iy + 1]
 				xval, yval = 0.5*(x0 + x1), 0.5*(y0 + y1)
 				val = self(xval, yval)
-				val = (val - vmin)/(vmax - vmin)
-				col = cmap(val, alpha=alpha)
-				ax.fill([x0, x1, x1, x0], [y0, y0, y1, y1], color=col)
+				try:
+					if not ignore(val):
+						val = (val - vmin)/(vmax - vmin)
+						col = cmap(val, alpha=alpha)
+						ax.fill([x0, x1, x1, x0], [y0, y0, y1, y1], color=col, **kwargs)
+				except TypeError:
+					if val != ignore:
+						val = (val - vmin)/(vmax - vmin)
+						col = cmap(val, alpha=alpha)
+						ax.fill([x0, x1, x1, x0], [y0, y0, y1, y1], color=col, **kwargs)
 
-	def integrate(self, xmin=-np.inf, xmax=np.inf, ymin=-np.inf, ymax=np.inf):
+	def integral(self, xmin=-np.inf, xmax=np.inf, ymin=-np.inf, ymax=np.inf):
 		"""
 		"""
 		x = np.unique(np.concatenate([self.x[(self.x >= xmin)&(self.x <= xmax)], [-np.inf, +np.inf]]))
