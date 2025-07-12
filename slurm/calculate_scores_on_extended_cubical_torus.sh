a3f91bf30f9fa991b1e4d2b88737d19667db9e03
Feodor <zimfv@yandex.ru>
2025-07-12 15:07:29 +0200

    Wrote slurm and started for extended torus model

---
+++ slurm/calculate_scores_on_extended_cubical_torus.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#
+#SBATCH --job-name=DepthStatsRCTE
+#SBATCH --output=logs/scores_on_extended_cubical_torus/output_%j.log
+#
+#SBATCH --time=72:00:00
+#SBATCH --mem=16G
+
+echo "SLURM_ARRAY_TASK_ID = '$SLURM_ARRAY_TASK_ID'"
+
+param_file="params/scores_on_cubical_torus.txt"
+param=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $param_file)
+
+dim=$(echo $param | cut -d ' ' -f 1)
+n=$(echo $param | cut -d ' ' -f 2)
+echo "PARAMS:"
+echo "dim=$dim"
+echo "n=$n"
+
+source venv/bin/activate
+
+python -u scripts/calculate_scores_on_extended_cubical_torus.py $dim $n
+
+deactivate
---
+++ starters/run_experiment_scores_on_extended_cubical_torus.py
@@ -0,0 +1,50 @@
+# system imports
+import sys
+from pathlib import Path
+
+# change root
+script_dir = Path(__file__).resolve().parent
+project_root = script_dir.parent
+sys.path.append(str(project_root))
+
+# main imports
+import os
+import subprocess
+import itertools
+
+
+# define params
+repeat_params = [
+    {'dim': 1, 'ns': list(range(3, 6, 1)), 'repeat': 3},
+    {'dim': 2, 'ns': list(range(3, 6, 1)), 'repeat': 3},
+    {'dim': 3, 'ns': list(range(3, 6, 1)), 'repeat': 3},
+]
+dims = list(itertools.chain.from_iterable([[params_dict['dim'] for _ in range(len(params_dict['ns'])*params_dict['repeat'])] for params_dict in repeat_params]))
+ns = list(itertools.chain.from_iterable([[n for n, _ in itertools.product(params_dict['ns'], range(params_dict['repeat']))] for params_dict in repeat_params]))
+
+params_txt = '\n'.join([f'{dim} {n}' for dim, n in zip(dims, ns)]) + '\n'
+
+# save params to file
+if not os.path.exists('params'):
+    os.makedirs('params')
+with open('params/scores_on_extended_cubical_torus.txt', 'w') as file:
+    file.write(params_txt)
+
+# create logs directory if not exists
+if not os.path.exists('logs/scores_on_extended_cubical_torus'):
+    os.makedirs('logs/scores_on_extended_cubical_torus')
+
+# Submit the SLURM array job and capture output
+result = subprocess.run(
+    ['sbatch', f'--array=0-{len(ns) - 1}', 'slurm/calculate_scores_on_extended_cubical_torus.sh'],
+    capture_output=True,
+    text=True
+)
+
+# Check for errors
+if result.returncode != 0:
+    print("Error submitting job:")
+    print(result.stderr)
+else:
+    print("Submission output:")
+    print(result.stdout)
