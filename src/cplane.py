569c69e9bc95b0235207cb8b3f8a725ea2cdc185
Feodor <zimfv@yandex.ru>
2025-08-12 19:13:50 +0200

    Move complex maps to a module. There are some issue killing core

---
+++ src/cplane.py
@@ -0,0 +1,197 @@
+import numpy as np
+
+from scipy.spatial import Delaunay
+import triangle
+
+
+def triangulate_complex_numbers(zs, fix_perimeter=True):
+    """
+    Returns the triangulation of the set of complex numbers on the complex plane
+
+    Parameters:
+    -----------
+    zs: array[complex]
+        The ordered boundary
+
+    fix_perimeter: bool
+        Adds the constrain, that there should be edges, connecting neihbour numbers in the order, if it's True
+        If Flase - just compute Delaunay triangulation
+
+    Returns:
+    --------
+    tri: list(tuple)
+        The triples of indices, which are the triangle vertices
+    """
+    points = np.transpose([np.real(zs), np.imag(zs)])
+    if not fix_perimeter:
+        tri = Delaunay(points)
+        tri = tri.simplices
+    else:
+        segments = np.transpose([np.arange(len(zs)), (np.arange(len(zs)) + 1)%len(zs)])
+        tri = triangle.triangulate(dict(vertices=points, segments=segments), "p")
+        assert (tri['vertices'] == points).all()
+        tri = tri['triangles']
+    return tri
+
+
+def get_triangle_representation(z, z1, z2, z3):
+    """
+    Represents the complex number z as linear combination of 3 given numbers z1, z2, z3
+
+    Parameters:
+    -----------
+    z: complex array
+        A complex number or an array of complex numbers to be represented in the triangle
+    
+    z1, z2, z3: complex numbers
+        Vertices of the triangle
+    
+    Returns:
+    --------
+    t: np.array
+        An array of shape (z.shape) + (3,) with coefficients for the representation of `z` in the triangle defined by `z1`, `z2`, `z3`
+    """
+    z = np.asarray(z)
+    a1 = np.real(z2) - np.real(z1)
+    b1 = np.real(z3) - np.real(z1)
+    c1 = np.real(z) - np.real(z1)
+    a2 = np.imag(z2) - np.imag(z1)
+    b2 = np.imag(z3) - np.imag(z1)
+    c2 = np.imag(z) - np.imag(z1)
+    det = a1 * b2 - a2 * b1
+    if np.any(det == 0):
+        raise ValueError("The triangle is degenerate")
+    t3 = (a1*c2 - a2*c1) / det
+    t2 = (b2*c1 - b1*c2) / det
+    t1 = 1 - t2 - t3
+    return np.array([t1, t2, t3]).transpose(tuple(np.arange(z.ndim) + 1) + (0, ))
+
+
+def map_triangle(z, z1_input, z2_input, z3_input, z1_output, z2_output, z3_output):
+    """
+    Represents the number z as linear combination of input triangle and linearly map this as represented in the output triangle
+
+    Parameters:
+    -----------
+    z: complex or complex array
+        The complex argument
+    
+    z1_input, z2_input, z3_input: complex
+        The vertices of the input triangle
+    
+    z1_output, z2_output, z3_output: complex
+        The vertices of the output triangle
+
+    Returns:
+    --------
+
+    """
+    t = get_triangle_representation(z, z1_input, z2_input, z3_input)
+    z_res = t@np.array([z1_output, z2_output, z3_output], dtype='complex')
+    return z_res
+
+
+def get_map_boundary_to_unit_circle(zs, angles=None):
+    """
+    Returns the function from complex to complex maping the given boundary to the unit cicrle (circumscribed unit circle polygon).
+    This triangulates the boundary. Maps each vertex of the boundary to the unit circle, 
+    and then maps numbers in these triangles as their linear representations 
+
+    Parameters:
+    -----------
+    zs: array[complex]
+        The ordered boundary
+    
+    angles: array[float]
+        The angles coresponding the points of boundary.
+        Should be the same length as zs
+        This will be defined automatically, if it's not given
+
+    Returns:
+    --------
+    map_boundary_to_unit_circle: function
+        Maps the boundary to a unit circle in the complex space 
+    """
+    if angles is None:
+        angles = 2*np.pi*np.arange(len(zs))/len(zs)
+    angles %= 2*np.pi
+
+    triangles = triangulate_complex_numbers(zs)
+    triangle_inputs = np.array([zs[tri] for tri in triangles])
+    triangle_outputs = np.array([np.exp(1j*angles[tri]) for tri in triangles])
+
+    def map_boundary_to_unit_circle(z):
+        z = np.asarray(z)
+        z_work = z.reshape(-1)
+        z_res = np.nan*z_work
+        for (z1_input, z2_input, z3_input), (z1_output, z2_output, z3_output) in zip(triangle_inputs, triangle_outputs):
+            m = get_triangle_representation(z_work, z1_input, z2_input, z3_input)
+            m = ((m >= 0) & (m <= 1)).all(axis=-1)
+            z_res[m] = map_triangle(z_work[m], z1_input, z2_input, z3_input, z1_output, z2_output, z3_output)
+        z_res = z_res.reshape(z.shape)
+        
+        return z_res
+    return map_boundary_to_unit_circle
+
+
+def get_map_changing_center(z0, alpha=0):
+    """
+    Returns the complex function, changing the center of the unit disk, but preserving the boundary unit circle:
+
+    .. math::
+
+        f(z) = (1-t(z))\cdot(z - z_0)\cdot e^{i\alpha} + t(z)\cdot z
+    
+    where the parameter $t(z) = \frac{|z - z_0|}{|e^{i\cdot \arg(z - z_0)} - z_0|}$.
+
+    Parameters:
+    -----------
+    z0: complex
+        The new center of the unit disk
+    
+    alpha: float
+        The angle to rotate the neighbourhood arround the center
+
+    Returns:
+    --------
+    map_changing_center: function
+        The complex function changing the center of the unit disk
+    """
+    def map_changing_center(z):
+        z = np.asarray(z)
+        t = np.abs(z - z0)/np.abs(np.exp(1j*np.angle(z - z0)) - z0)
+        res = t*z + (1 - t)*(z - z0)*np.exp(1j*alpha)
+        return res
+    return map_changing_center
+
+
+def get_map_disk_to_unit(z0, zs, angles=None):
+    """
+    Returns the function maping disk to the unit disk and seting the center
+
+    Parameters:
+    -----------
+    z0: complex
+        The new center of the unit disk
+    
+    zs: array[complex]
+        The ordered boundary
+    
+    angles: array[float]
+        The angles coresponding the points of boundary.
+        Should be the same length as zs
+        This will be defined automatically, if it's not given
+
+    Returns:
+    --------
+    map_disk_to_unit: function
+    """
+    if angles is None:
+        angles = 2*np.pi*np.arange(len(zs))/len(zs)
+
+    alpha = np.angle(zs - z0)[np.argmin(angles%(2*np.pi))]
+    f1 = get_map_boundary_to_unit_circle(zs, angles)    
+    f2 = get_map_changing_center(f1(z0), alpha)
+    def map_disk_to_unit(z):
+        return f2(f1(z))
+    return map_disk_to_unit
---
+++ tests/test_complex_maps.py
@@ -0,0 +1,147 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage
+
+from src.cplane import get_map_disk_to_unit
+
+
+from skimage import measure
+
+
+def extract_ordered_boundary(mask, return_countor: bool=False):
+    """
+    Exclude the ordered boundary as list of tuples 
+
+    Parameters:
+    -----------
+    mask: np.array ndim=2
+        The binary mask
+    
+    return_countor: bool:
+        Returns the float coordinates between pixels if True
+        Returns the coordinates of the pixesl if False
+
+    Returns:
+    --------
+    ordered_boundary: list[tuple]
+        coordinates of the boundary
+    """
+    contours = measure.find_contours(mask.astype(float), level=0.5)
+    if not contours:
+        raise ValueError("Could not find a border")
+    
+    # Take the longest border if there are many
+    contour = max(contours, key=len)
+    if return_countor:
+        return contour
+
+    # Choose the border pixels
+    ordered_boundary = [(int(round(y)), int(round(x))) for y, x in contour]
+    ordered_boundary = [(y, x) for y, x in contour]
+    ordered_boundary = []
+    for y, x, in contour:
+        if y%1 == 0:
+            y, x = int(y), int(x)
+            if not mask[y, x]:
+                x += 1
+        else:
+            y, x = int(y), int(x)
+            if not mask[y, x]:
+                y += 1
+        if (y, x) not in ordered_boundary:
+            ordered_boundary.append((y, x))
+    
+
+    return ordered_boundary
+
+
+def test1():
+    zs = np.array([-2j, 2-1j, 3+1j, 2+3j, 2j, -2+3j, -1+1j, -2-1j])
+    z_center = 1 - 1j
+
+    f1 = get_map_disk_to_unit(z_center, zs)
+
+
+    x = np.linspace(-4, 4, 101)
+    y = np.linspace(-4, 4, 101)
+    z0 = x.reshape(1, -1) + 1j*y.reshape(-1, 1)
+    z1 = f1(z0)
+
+    zx = np.append(zs, zs[0]).real
+    zy = np.append(zs, zs[0]).imag
+    zx = np.searchsorted(x, zx)
+    zy = np.searchsorted(y, zy)
+
+
+    fig, axs = plt.subplots(2, 4, figsize=(15, 10))
+    for (i_col, repr), (i_row, z) in itertools.product(enumerate([np.real, np.imag, np.abs, np.angle]), enumerate([z0, z1])):
+        axs[i_row, i_col].set_title(f'{repr.__name__}')
+        img = repr(z)
+        cmap = plt.get_cmap().copy()
+        cmap.set_bad(color='gray')
+        sm = axs[i_row, i_col].imshow(img, cmap=cmap, origin='lower')
+        plt.colorbar(sm, ax=axs[i_row, i_col])
+
+        axs[i_row, i_col].plot(zx, zy, color='red', linestyle='--')
+        axs[i_row, i_col].set_xticks(np.linspace(0, len(x)-1, 5).astype(int), x[np.linspace(0, len(x)-1, 5).astype(int)])
+        axs[i_row, i_col].set_yticks(np.linspace(0, len(y)-1, 5).astype(int), y[np.linspace(0, len(y)-1, 5).astype(int)])
+        axs[i_row, i_col].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def test2():
+    mask = np.zeros([16, 12])
+    mask[2, 3:6] = 1
+    mask[3, 2:9] = 1
+    mask[4, 3:10] = 1
+    mask[5:8, 8] = 1
+    mask[5:11, 4:8] = 1
+    mask[8:11, 3] = 1
+    mask[11, 2:9] = 1
+    mask[12, 3:10] = 1
+    mask[13, 6:9] = 1
+
+    mask = ndimage.zoom(mask, (4, 4), order=1, grid_mode=True).astype(bool).astype(int)
+    #mask = ndimage.zoom(mask, (8, 8), order=1, grid_mode=True).astype(bool).astype(int)
+
+
+    center_x, center_y = np.argwhere(mask)[np.random.randint(mask.sum())]
+
+    boundary = extract_ordered_boundary(mask, return_countor=True)
+    boundary_x, boundary_y = np.transpose(boundary)
+    boundary_angles = np.arange(len(boundary))/len(boundary) * 2*np.pi
+
+
+    zs = boundary_x + 1j*boundary_y
+    z_center = center_x + 1j*center_y
+    
+    f = get_map_disk_to_unit(z_center, zs)
+
+    cmap = plt.get_cmap().copy()
+    cmap.set_bad(color='gray')
+
+    fig, axs = plt.subplots(2, 4, figsize=(16, 8))
+    for (i_col, repr), (i_row, z) in itertools.product(enumerate([np.real, np.imag, np.abs, np.angle]), enumerate([z0, f(z0)])):
+        axs[i_row, i_col].set_title(f'{repr.__name__}')
+        img = repr(z)
+        
+        sm = axs[i_row, i_col].imshow(img, cmap=cmap, origin='lower')
+        plt.colorbar(sm, ax=axs[i_row, i_col])
+
+        axs[i_row, i_col].plot(zx, zy, color='red', linestyle='--', zorder=1)
+        axs[i_row, i_col].scatter(center_x, center_y, color='red', marker='*', zorder=2)
+        axs[i_row, i_col].set_xticks(np.linspace(0, len(x)-1, 5).astype(int), x[np.linspace(0, len(x)-1, 5).astype(int)])
+        axs[i_row, i_col].set_yticks(np.linspace(0, len(y)-1, 5).astype(int), y[np.linspace(0, len(y)-1, 5).astype(int)])
+        axs[i_row, i_col].grid(True)
+
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    test1()
+    #test2() # there is some core issue
